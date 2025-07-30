96af0439a98455c574b5f8775d69a1e1822184f5
VathareVinayak <vinayak.vathare2004@gmail.com>
2025-07-30 22:55:12 +0530

    routes added - Shop App

--- cartique/shop/urls.py
+++ cartique/shop/urls.py
@@ -1,5 +1,12 @@
 from django.urls import path
 from . import views 
 urlpatterns = [
- path('',views.index , name='ShapHome')   
+ path('',views.index , name='ShopHome'),
+ path('about/',views.about , name='aboutUs'),
+ path('contact/',views.contact , name='contact'),
+ path('tracker/',views.tracker , name='tracker'),
+ path('search/',views.search , name='search'),
+ path('productview/',views.productView , name='productview'),
+ path('checkout/',views.checkout , name='Checkout'),
+ 
 ]
--- cartique/shop/views.py
+++ cartique/shop/views.py
@@ -1,6 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
-
+#index
 def index(request):
     return render(request , 'shop/index.html')
+#about
+def about(request):
+    return HttpResponse("This is the about page. We are the service providers to custermers")
+#contact
+def contact(request):
+    return HttpResponse("This is the contact page. We are the service providers to custermers")
+#tracking page 
+def tracker(request):
+    return HttpResponse("This is the tracker page. We are the service providers to you ,Check FAQ's here ")
+
+def search(request):
+    return HttpResponse("This is the search page. We are the service providers to custermers")
+
+def productView(request):
+    return HttpResponse("This is the productView page. We are the service providers to custermers")
+
+def checkout(request):
+    return HttpResponse("This is the checkout page. We are the service providers to custermers")
